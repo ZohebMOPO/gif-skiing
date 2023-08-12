284cfdb46fd95d5d04880f1edcf8de3d15fcc2c8
Siddharth <siddharth99c@gmail.com>
2023-08-12 23:28:59 +0530

    feat: adds basic ui

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	output: 'export',
+	images: { unoptimized: true },
+	experimental: {
+		urlImports: [
+			"https://framer.com/m/",
+			"https://framerusercontent.com/",
+			"https://ga.jspm.io/",
+			"https://jspm.dev/"
+		]
+	}
 }
 
 module.exports = nextConfig
