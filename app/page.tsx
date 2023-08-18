f61e643b290ce0d0b453c859964291c3711d20ca
Siddharth <siddharth99c@gmail.com>
2023-08-18 16:06:26 +0530

    feat: gifski binary cleanup and permissions

--- app/page.tsx
+++ app/page.tsx
@@ -6,21 +6,31 @@ import { listen } from "@tauri-apps/api/event";
 import Button from "https://framer.com/m/Button-8lyt.js@AYILo02jKZzU30gyM75p";
 
 export default function Home() {
-  const Trigger = async () => {
-    const commandArguments = "--fps 10 --width 320 -o anim.gif video.mp4";
+  const convertVideoToGif = async (sourceVideo: String) => {
+    const commandArguments = `--fps 10 --width 320 -o anim.gif ${sourceVideo}`;
     const command = Command.sidecar(
-      "bin/gifski-1.11.0/win/gifski",
+      "bin/gifski",
       commandArguments.split(" ")
     );
     const output = await command.execute();
     console.log({ output });
   };
-  Trigger();
+
   // Maybe wiser to handle this event directly from Rust instead of calling it from JS.
   const fileDropListener = async () => {
     return await listen("tauri://file-drop", (event) => {
-      const filePath = event.payload;
-      console.log(filePath);
+      const filePaths = event.payload;
+
+      let source
+
+      if (filePaths.length > 1) {
+        source = filePaths.join(" ")
+        console.log(source);
+      } else {
+        source = filePaths
+      }
+
+      convertVideoToGif(source);
     });
   };
 
@@ -43,3 +53,12 @@ export default function Home() {
     </main>
   );
 }
+
+
+
+
+// signal: null
+
+// stderr: "[swscaler @ 0x1301d0000] [swscaler @ 0x1301e0000] No accelerated colorspace conversion found from yuv420p to rgba.↵[swscaler @ 0x1301d0000…"
+
+// stdout: "\r↵Frame 1 / 137  _..........................................................  14s \r↵798KB GIF; Frame 31 / 137…"
